15682541ebb88a36b1348150f44715319708550c
Aleksandar Budjakoski <94196244+aleksandarbudzakoski@users.noreply.github.com>
2023-01-22 16:50:47 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/exam/example/service/impl/ProductServiceImpl.java
@@ -0,0 +1,80 @@
+package mk.ukim.finki.wp.exam.example.service.impl;
+
+import mk.ukim.finki.wp.exam.example.model.Category;
+import mk.ukim.finki.wp.exam.example.model.Product;
+import mk.ukim.finki.wp.exam.example.model.exceptions.InvalidProductIdException;
+import mk.ukim.finki.wp.exam.example.repository.CategoryRepository;
+import mk.ukim.finki.wp.exam.example.repository.ProductRepository;
+import mk.ukim.finki.wp.exam.example.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Product> listAllProducts() {
+        return this.productRepository.findAll();
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return this.productRepository.findById(id).orElseThrow(InvalidProductIdException::new);
+    }
+
+    @Override
+    public Product create(String name, Double price, Integer quantity, List<Long> categories) {
+        List<Category> categoriesNames = this.categoryRepository.findAllById(categories);
+
+        Product product = new Product(name,price,quantity,categoriesNames);
+        return this.productRepository.save(product);
+    }
+
+    @Override
+    public Product update(Long id, String name, Double price, Integer quantity, List<Long> categories) {
+        Product product = this.findById(id);
+        product.setName(name);
+        product.setPrice(price);
+        product.setQuantity(quantity);
+
+        List<Category> categoriesNames = this.categoryRepository.findAllById(categories);
+        product.setCategories(categoriesNames);
+
+        return this.productRepository.save(product);
+
+    }
+
+    @Override
+    public Product delete(Long id) {
+        Product product = this.findById(id);
+        this.productRepository.delete(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> listProductsByNameAndCategory(String name, Long categoryId) {
+        Category category = categoryId!=null ? this.categoryRepository.findById(categoryId).orElse(null): null;
+        if(name!=null && category!=null)
+            return this.productRepository.findAllByNameLikeAndCategoriesContaining("%"+name+"%",category);
+        else if (name!=null)
+            return this.productRepository.findAllByNameLike("%"+name+"%");
+        else if(category!=null)
+            return this.productRepository.findAllByCategoriesContains(category);
+        else
+            return this.productRepository.findAll();
+
+//        return this.productRepository.findAll().stream()
+//                .filter(i->i.getName().contains(name) && i.getCategories().stream().anyMatch(j->j.getId().equals(categoryId)))
+//                .collect(Collectors.toList());
+    }
+}
